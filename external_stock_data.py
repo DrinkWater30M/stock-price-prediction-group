f211f18a84476fcd2fed1e1bcfb27e2a06bbb73f
Nguyen Van Loi <nguyenvanloi10101@gmail.com>
2023-06-27 00:28:16 +0700

    predection using rnn algorithm

--- external_stock_data.py
+++ external_stock_data.py
@@ -3,13 +3,13 @@ from datetime import date, timedelta
 
 def getStockData(stock, start_date, end_date):
   # get data
-  data = yf.download(stock, start_date, end_date)
+  yf_clone = yf
+  data = yf_clone.download(stock, start_date, end_date)
   return data
 
 def getStockDataToNow(stock, days):
   end_date = date.today()
   start_date = end_date - timedelta(days)
-  print(start_date)
   data = getStockData(stock, start_date, end_date)
 
   return data
